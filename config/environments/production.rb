75d52d99e01a3c76031337f1e945f76e95e50c74
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-02-16 21:41:50 +1100

    Added email support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => "587",
+      :domain => "reporton.me",
+      :user_name => ENV['EMAIL_USER'],
+      :password => ENV['EMAIL_PASSWORD'],
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
 end
