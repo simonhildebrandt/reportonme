6376bb7c870261773f45eda915139e4cc231935c
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-02-16 21:48:49 +1100

    Tweaked email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => "587",
-      :domain => "reporton.me",
+      :domain => "gmail.com",
       :user_name => ENV['EMAIL_USER'],
       :password => ENV['EMAIL_PASSWORD'],
       :authentication => "plain",
