78107b7e6cd73597871bf7b7482d16c6c3c483cf
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-02-16 22:02:21 +1100

    Configured email

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,20 @@ module Reportonme
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.action_mailer.default_url_options = { host: 'reporton.me' }
+    config.action_mailer.default_url_options = { host: 'reportonme.herokuapp.com' } #reporton.me
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = {
+        :enable_starttls_auto => true,
+        :address        => 'smtp.gmail.com',
+        :port           => 587,
+        :domain         => 'gmail.com',
+        :authentication => :plain,
+        :user_name      => ENV['EMAIL_USER'],
+        :password       => [ENV['EMAIL_PASSWORD']
+      }
+
   end
 end
